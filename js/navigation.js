c475a3afbe6cd62c45edccb713828dd30c37ef33
Peter Kriška <pekriska@gmail.com>
2019-03-15 14:37:58 +0100

    1.00.09

--- js/navigation.js
+++ js/navigation.js
@@ -6,13 +6,31 @@
  */
 
 $ = jQuery.noConflict(true);
+var breakpoint = 767;
 
 $(document).ready( function () {
 
-var breakpoint = 767;
+var nav1 = document.getElementsByClassName('front-nav');
+//getElementsByClass return array
+var nav = nav1[0];
 
 if ($(document).width() <= breakpoint){
   $("#primary-menu").css("display","none");
+  if(typeof nav !== "undefined"){
+    nav.style.background = "#2b2b2b";
+  }
+}else{
+	if(typeof nav !== "undefined"){
+		window.onscroll = function(){
+			if (window.pageYOffset >100){
+				nav.style.background = "#2b2b2b";
+				nav.style.transition = ".5s";
+			}
+			else {
+				nav.style.background = "transparent";
+			}
+		}
+  }
 
 }
 
@@ -37,33 +55,35 @@ if ($(document).width() <= breakpoint){
     
     if ($(document).width() > breakpoint){
       $("#primary-menu").css("display","block");
+      if (window.pageYOffset >100){
+        nav.style.background = "#2b2b2b";
+      }else{
+        nav.style.background = "transparent";
+      }
+
+      if(typeof nav !== "undefined"){
+        window.onscroll = function(){
+          if (window.pageYOffset >100){
+            nav.style.background = "#2b2b2b";
+            nav.style.transition = ".5s";
+          }
+          else {
+            nav.style.background = "transparent";
+          }
+        }
+      }
 		}else{
       $("#primary-menu").css("display","none");
       $('.menu-toggle').removeClass("is-active");
+      if(typeof nav !== "undefined"){
+        nav.style.background = "#2b2b2b";
+      }
+      
     }
 
 		sizeOfScreen = $(window).width();
   });
+  
 
 });
 
-
-( function() {
-	
-	var nav1 = document.getElementsByClassName('front-nav');
-	//getElementsByClass return array
-	var nav = nav1[0];
-
-	if(typeof nav !== "undefined"){
-		window.onscroll = function(){
-			if (window.pageYOffset >100){
-				nav.style.background = "#2b2b2b";
-				nav.style.transition = ".5s";
-			}
-			else {
-				nav.style.background = "transparent";
-			}
-		}
-	}
-
-} )();
